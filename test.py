7d48106eb1544c9dc8284a8ad9e04d93ca6f6695
zero <kingstarcraft@foxmail.com>
2020-01-16 19:30:16 +0800

    test all function without train.py

--- test.py
+++ test.py
@@ -13,74 +13,77 @@ import wavenet
 flags = tf.app.flags
 flags.DEFINE_string('config_path', 'config/english-28.json', 'Directory to config.')
 flags.DEFINE_string('dataset_path', 'data/v28/test.record', 'Path to wave file.')
-flags.DEFINE_string('device', '/gpu:1', 'the device used to test.')
+flags.DEFINE_integer('device', 0, 'the device used to test.')
 flags.DEFINE_string('ckpt_dir', 'model/v28', 'Path to directory holding a checkpoint.')
 FLAGS = flags.FLAGS
 
 
 def main(_):
   utils.load(FLAGS.config_path)
-  with tf.device(FLAGS.device):
-    test_dataset = dataset.create(FLAGS.dataset_path, repeat=False, batch_size=1)
-    waves = tf.reshape(tf.sparse.to_dense(test_dataset[0]), shape=[1, -1, utils.Data.num_channel])
-    labels = tf.sparse.to_dense(test_dataset[1])
-    sequence_length = tf.cast(test_dataset[2], tf.int32)
-    vocabulary = tf.constant(utils.Data.vocabulary)
-    labels = tf.gather(vocabulary, labels)
-    logits = wavenet.bulid_wavenet(waves, len(utils.Data.vocabulary))
-    decodes, _ = tf.nn.ctc_beam_search_decoder(
-      tf.transpose(logits, perm=[1, 0, 2]), sequence_length, merge_repeated=False)
-    outputs = tf.gather(vocabulary,  tf.sparse.to_dense(decodes[0]))
-    save = tf.train.Saver()
+  os.environ["CUDA_VISIBLE_DEVICES"] = str(FLAGS.device)
+ # with tf.device(FLAGS.device):
+  test_dataset = dataset.create(FLAGS.dataset_path, repeat=False, batch_size=1)
+  waves = tf.reshape(tf.sparse.to_dense(test_dataset[0]), shape=[1, -1, utils.Data.num_channel])
+  labels = tf.sparse.to_dense(test_dataset[1])
+  sequence_length = tf.cast(test_dataset[2], tf.int32)
+  vocabulary = tf.constant(utils.Data.vocabulary)
+  labels = tf.gather(vocabulary, labels)
+  logits = wavenet.bulid_wavenet(waves, len(utils.Data.vocabulary))
+  decodes, _ = tf.nn.ctc_beam_search_decoder(
+    tf.transpose(logits, perm=[1, 0, 2]), sequence_length, merge_repeated=False)
+  outputs = tf.gather(vocabulary,  tf.sparse.to_dense(decodes[0]))
+  save = tf.train.Saver()
 
-    evalutes = {}
-    if os.path.exists(FLAGS.ckpt_dir + '/evalute.json'):
-      evalutes = json.load(open(FLAGS.ckpt_dir + '/evalute.json', encoding='utf-8'))
+  evalutes = {}
+  if os.path.exists(FLAGS.ckpt_dir + '/evalute.json'):
+    evalutes = json.load(open(FLAGS.ckpt_dir + '/evalute.json', encoding='utf-8'))
 
-    with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
-      sess.run(tf.global_variables_initializer())
-      status = 0
-      while True:
-        filepaths = glob.glob(FLAGS.ckpt_dir + '/*.index')
-        for filepath in filepaths:
-          model_path = os.path.splitext(filepath)[0]
-          uid = os.path.split(model_path)[-1]
-          if uid in evalutes:
-            if status == 1:
-              continue
-          else:
-            status = 2
-            save.restore(sess, model_path)
-            evalutes[uid] = {}
-            tps, preds, poses, count = 0, 0, 0, 0
-            while True:
-              try:
-                count += 1
-                y, y_ = sess.run((labels, outputs))
-                y = utils.cvt_np2string(y)
-                y_ = utils.cvt_np2string(y_)
-                tp, pred, pos = utils.evalutes(y, y_)
-                tps += tp
-                preds += pred
-                poses += pos
-                if count % 1000 == 0:
-                  glog.info('processed %d: tp=%d, pred=%d, pos=%d.' % (count, tps, preds, poses))
-              except:
-                if count % 1000 != 0:
-                  glog.info('processed %d: tp=%d, pred=%d, pos=%d.' % (count, tps, preds, poses))
-                break
+  config = tf.ConfigProto(allow_soft_placement=True)
+  config.gpu_options.allow_growth = True
+  with tf.Session(config=config) as sess:
+    sess.run(tf.global_variables_initializer())
+    status = 0
+    while True:
+      filepaths = glob.glob(FLAGS.ckpt_dir + '/*.index')
+      for filepath in filepaths:
+        model_path = os.path.splitext(filepath)[0]
+        uid = os.path.split(model_path)[-1]
+        if uid in evalutes:
+          if status == 1:
+            continue
+        else:
+          status = 2
+          save.restore(sess, model_path)
+          evalutes[uid] = {}
+          tps, preds, poses, count = 0, 0, 0, 0
+          while True:
+            try:
+              count += 1
+              y, y_ = sess.run((labels, outputs))
+              y = utils.cvt_np2string(y)
+              y_ = utils.cvt_np2string(y_)
+              tp, pred, pos = utils.evalutes(y, y_)
+              tps += tp
+              preds += pred
+              poses += pos
+              if count % 1000 == 0:
+                glog.info('processed %d: tp=%d, pred=%d, pos=%d.' % (count, tps, preds, poses))
+            except:
+              if count % 1000 != 0:
+                glog.info('processed %d: tp=%d, pred=%d, pos=%d.' % (count, tps, preds, poses))
+              break
 
-            evalutes[uid]['tp'] = tps
-            evalutes[uid]['pred'] = pred
-            evalutes[uid]['pos'] = pos
-            evalutes[uid]['f1'] = 2 * tps / (preds + poses + 1e-20)
-            json.dump(evalutes, open(FLAGS.ckpt_dir + '/evalute.json', mode='w', encoding='utf-8'))
-          evalute = evalutes[uid]
-          glog.info('Evalute %s: tp=%d, pred=%d, pos=%d, f1=%f.' %
-                    (uid, evalute['tp'], evalute['pred'], evalute['pos'], evalute['f1']))
-        if status == 1:
-          time.sleep(60)
-        status = 1
+          evalutes[uid]['tp'] = tps
+          evalutes[uid]['pred'] = pred
+          evalutes[uid]['pos'] = pos
+          evalutes[uid]['f1'] = 2 * tps / (preds + poses + 1e-20)
+          json.dump(evalutes, open(FLAGS.ckpt_dir + '/evalute.json', mode='w', encoding='utf-8'))
+        evalute = evalutes[uid]
+        glog.info('Evalute %s: tp=%d, pred=%d, pos=%d, f1=%f.' %
+                  (uid, evalute['tp'], evalute['pred'], evalute['pos'], evalute['f1']))
+      if status == 1:
+        time.sleep(60)
+      status = 1
 
 
 if __name__ == '__main__':
--- train.py
+++ train.py
@@ -11,7 +11,7 @@ flags.DEFINE_string('dataset_path', 'data/v28/train.record', 'Filepath to train 
 flags.DEFINE_integer('batch_size', 32, 'Batch size of train.')
 flags.DEFINE_integer('display', 100, 'Step to display loss.')
 flags.DEFINE_integer('snapshot', 1000, 'Step to save model.')
-flags.DEFINE_string('device', '/gpu:0', 'The device used to train.')
+flags.DEFINE_integer('device', 0, 'The device used to train.')
 flags.DEFINE_string('pretrain_dir', 'pretrain', 'Filepath to test record.')
 flags.DEFINE_string('ckpt_path', 'model/v28/ckpt', 'Path to directory holding a checkpoint.')
 flags.DEFINE_float('learning_rate', 0.01, 'Learning rate of train.')
@@ -19,54 +19,54 @@ FLAGS = flags.FLAGS
 
 
 def main(_):
+  os.environ["CUDA_VISIBLE_DEVICES"] = str(FLAGS.device)
   utils.load(FLAGS.config_path)
-  with tf.device(FLAGS.device):
-    global_step = tf.train.get_or_create_global_step()
-    train_dataset = dataset.create(FLAGS.dataset_path, FLAGS.batch_size, repeat=True)
+  global_step = tf.train.get_or_create_global_step()
+  train_dataset = dataset.create(FLAGS.dataset_path, FLAGS.batch_size, repeat=True)
 
-    waves = tf.reshape(tf.sparse.to_dense(train_dataset[0]), shape=[FLAGS.batch_size, -1, utils.Data.num_channel])
-    labels = tf.cast(train_dataset[1], tf.int32)
-    sequence_length = tf.cast(train_dataset[2], tf.int32)
-    logits = wavenet.bulid_wavenet(waves, len(utils.Data.vocabulary) + 1, is_training=True)
-    loss = tf.reduce_mean(tf.nn.ctc_loss(labels, logits, sequence_length, time_major=False))
+  waves = tf.reshape(tf.sparse.to_dense(train_dataset[0]), shape=[FLAGS.batch_size, -1, utils.Data.num_channel])
+  labels = tf.cast(train_dataset[1], tf.int32)
+  sequence_length = tf.cast(train_dataset[2], tf.int32)
+  logits = wavenet.bulid_wavenet(waves, len(utils.Data.vocabulary) + 1, is_training=True)
+  loss = tf.reduce_mean(tf.nn.ctc_loss(labels, logits, sequence_length, time_major=False))
 
-    vocabulary = tf.constant(utils.Data.vocabulary)
-    decodes, _ = tf.nn.ctc_beam_search_decoder(tf.transpose(logits, [1, 0, 2]), sequence_length, merge_repeated=False)
-    outputs = tf.gather(vocabulary, tf.sparse.to_dense(decodes[0]))
-    labels = tf.gather(vocabulary, tf.sparse.to_dense(labels))
+  vocabulary = tf.constant(utils.Data.vocabulary)
+  decodes, _ = tf.nn.ctc_beam_search_decoder(tf.transpose(logits, [1, 0, 2]), sequence_length, merge_repeated=False)
+  outputs = tf.gather(vocabulary, tf.sparse.to_dense(decodes[0]))
+  labels = tf.gather(vocabulary, tf.sparse.to_dense(labels))
 
-    update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
-    with tf.control_dependencies(update_ops):
-      optimize = tf.train.AdamOptimizer(learning_rate=FLAGS.learning_rate).minimize(loss=loss, global_step=global_step)
+  update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+  with tf.control_dependencies(update_ops):
+    optimize = tf.train.AdamOptimizer(learning_rate=FLAGS.learning_rate).minimize(loss=loss, global_step=global_step)
 
-    save = tf.train.Saver(max_to_keep=1000)
-    config = tf.ConfigProto(allow_soft_placement=True)
-    config.gpu_options.allow_growth = True
-    with tf.Session(config=config) as sess:
-      sess.run(tf.global_variables_initializer())
+  save = tf.train.Saver(max_to_keep=1000)
+  config = tf.ConfigProto(allow_soft_placement=True)
+  config.gpu_options.allow_growth = True
+  with tf.Session(config=config) as sess:
+    sess.run(tf.global_variables_initializer())
 
-      if os.path.exists(FLAGS.pretrain_dir) and len(os.listdir(FLAGS.pretrain_dir)) > 0:
-        save.restore(sess, tf.train.latest_checkpoint(FLAGS.pretrain_dir))
-      ckpt_dir = os.path.split(FLAGS.ckpt_path)[0]
-      if not os.path.exists(ckpt_dir):
-        os.makedirs(ckpt_dir)
-      if len(os.listdir(ckpt_dir)) > 0:
-        save.restore(sess, tf.train.latest_checkpoint(ckpt_dir))
+    if os.path.exists(FLAGS.pretrain_dir) and len(os.listdir(FLAGS.pretrain_dir)) > 0:
+      save.restore(sess, tf.train.latest_checkpoint(FLAGS.pretrain_dir))
+    ckpt_dir = os.path.split(FLAGS.ckpt_path)[0]
+    if not os.path.exists(ckpt_dir):
+      os.makedirs(ckpt_dir)
+    if len(os.listdir(ckpt_dir)) > 0:
+      save.restore(sess, tf.train.latest_checkpoint(ckpt_dir))
 
-      losses, tps, preds, poses = 0, 0, 0, 0
-      while True:
-        gp, ll, ot, ls, _ = sess.run((global_step, labels, outputs, loss, optimize))
-        tp, pred, pos = utils.evalutes(utils.cvt_np2string(ot), utils.cvt_np2string(ll))
-        tps += tp
-        losses += ls
-        preds += pred
-        poses += pos
-        if gp / FLAGS.display == 0:
-          glog.info("Step %d: loss=%f, tp=%d, pos=%d, pred=%d, f1=%d." %
-                    (gp, losses if gp == 0 else (losses / FLAGS.display), tps, preds, poses, 2 * tps / (preds + poses + 1e-10)))
-          losses, tps, preds, poses = 0, 0, 0, 0
-        if gp / FLAGS.snapshot == 0 and gp != 0:
-          save.save(sess, FLAGS.ckpt_path)
+    losses, tps, preds, poses = 0, 0, 0, 0
+    while True:
+      gp, ll, ot, ls, _ = sess.run((global_step, labels, outputs, loss, optimize))
+      tp, pred, pos = utils.evalutes(utils.cvt_np2string(ot), utils.cvt_np2string(ll))
+      tps += tp
+      losses += ls
+      preds += pred
+      poses += pos
+      if gp / FLAGS.display == 0:
+        glog.info("Step %d: loss=%f, tp=%d, pos=%d, pred=%d, f1=%d." %
+                  (gp, losses if gp == 0 else (losses / FLAGS.display), tps, preds, poses, 2 * tps / (preds + poses + 1e-10)))
+        losses, tps, preds, poses = 0, 0, 0, 0
+      if gp / FLAGS.snapshot == 0 and gp != 0:
+        save.save(sess, FLAGS.ckpt_path)
 
 
 if __name__ == '__main__':
